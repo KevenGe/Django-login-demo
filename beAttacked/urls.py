3e0ac0cbd7608842eaa97137754a5499f371f80e
Keven Ge <1985996902@qq.com>
2020-09-19 20:52:15 +0800

    delete: 删除冗余代码

--- beAttacked/urls.py
+++ beAttacked/urls.py
@@ -1,4 +1,4 @@
-"""testttt URL Configuration
+"""beAttacked URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("user/", include("user.urls")),
-    path("", include("weba.urls")),
     path('admin/', admin.site.urls),
 ]
--- beAttacked/wsgi.py
+++ beAttacked/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for testttt project.
+WSGI config for beAttacked project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testttt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beAttacked.settings')
 
 application = get_wsgi_application()
